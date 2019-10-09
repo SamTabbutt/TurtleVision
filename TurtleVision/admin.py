1857a5d969a21a7527134db9d95e48507b9720c1
SamTabbutt <samtabbutt@gmail.com>
2019-10-09 11:31:50 -0400

    update models and admin rights

--- TurtleVision/admin.py
+++ TurtleVision/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Session, Movie
+from .models import Session, Movie, SecondDat
 
 admin.site.register(Session)
 admin.site.register(Movie)
+admin.site.register(SecondDat)
--- TurtleVision/models.py
+++ TurtleVision/models.py
@@ -1,15 +1,68 @@
 from django.db import models
 
-# Create your models here.
+# The goal of the application is to:
+#	1) videos and .csv log uploaded by admin user
+# 	2) database containing frames updated by common user
+#	3) admin runs automatic analysis on video
+
+
+
+
+# Session: when the TurtleCam is deployed, everything that happens within the timeframe between 
+# when the turtle is deployed and the camera stops recording is in the context of a single session
+# The session will all exist with a single turtle and recorded on a date. It is also worth
+# Tracking when it was published and where the turtlecam was deployed
 
 class Session(models.Model):
 	session_id = models.CharField(max_length=20)
+	session_num = models.IntegerField(default=0)
 	turtle_id = models.IntegerField(default=0)
 	record_date = models.DateField('date recorded')
 	pub_date = models.DateTimeField('date published')
+	loc_name = models.CharField(default='CEI',max_length=15)
+
+	def __str__(self):
+        	return self.session_id
+
+
+
+# Movie: the session contains about twenty individual movie files that comprise the entirety of
+# the session. Each movie is associated with one session, but a single session may contain twenty movies
+# video_path will refer to the folder containing the movie file. video_title will contain the name of the file
+# video_ ID will be a unique video id containing information of the session and turtle
+# video_length is important to match up data points with the row of information
 
 class Movie(models.Model):
 	session = models.ForeignKey(Session, on_delete=models.CASCADE)
 	video_path = models.CharField(max_length=200)
 	video_title = models.CharField(max_length=20)
 	video_id = models.CharField(max_length=50)
+	video_length = models.TimeField(blank=True, null=True)
+
+	def __str__(self):
+        	return self.video_id
+
+
+
+
+# SecondDat: this datatype is considered a single "row" in the analysis of a single turtlecam video
+# These instances were previously filled out manually using excel to complete the analysis
+# The goal of TurtleVision is to automate this process. This datatype will be filled out
+# Manually via the "Train" page of the website when an administrator is on the page
+# This datatype contains the information derived from the .csv file uploaded by admin along with
+# session. 
+
+class SecondDat(models.Model):
+	session = models.ForeignKey(Session, on_delete=models.CASCADE)
+	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+	sec_id = models.CharField(max_length=50)
+	depth = models.DecimalField(max_digits=3, decimal_places=1)
+	temp = models.DecimalField(max_digits=3, decimal_places=1)
+	seg_time = models.TimeField(blank=True, null=True)
+	session_time = models.TimeField(blank=True, null=True)
+
+	def __str__(self):
+        	return self.sec_id
+
+
+
